c65918ae715e1cb6ee8272a5e95e5c4073675a44
gyan011 <158264540+gyan011@users.noreply.github.com>
2025-04-12 20:35:26 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://gyanranjan9661:gyan7462@cluster0.qq81xhn.mongodb.net/food-delivery')
+    await mongoose.connect(process.env.MONGO_URL)
     .then( () => {
         console.log("DB connected");
     })
